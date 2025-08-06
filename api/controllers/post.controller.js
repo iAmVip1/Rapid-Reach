a738818d8f9202ff3110b9eb54f12ed0eec69d06
iAmVip1 <gp16373@gmail.com>
2025-08-06 21:40:12 +0545

    post create api

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,14 @@
+
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const createPost = async (req, res, next) =>{
+    try {
+        const post = await Post.create(req.body);
+        return res.status(201).json(post);
+
+    } catch (error) {
+        next(error);
+        
+    }
+}
---
+++ api/models/post.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    postname: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+
+    },
+    longitude: {
+      type: String,
+      
+    },
+    latitude: {
+      type: String,
+      
+    },
+    phoneNumber1: {
+      type: Number,
+      required: true,
+    },
+    phoneNumber2: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+    userType: {
+      type: String,
+      required: true,
+    },
+    userMail: {
+      type: String,
+      required: true,
+    },
+    userImage: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
