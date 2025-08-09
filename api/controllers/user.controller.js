154d2a88b3e70455805e9c8177ae491c6276ddaf
iAmVip1 <gp16373@gmail.com>
2025-08-09 14:39:05 +0545

    get posts api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
+import Post from '../models/post.model.js';
 
 export const test = (req, res) => {
     res.json({message: 'API is working'})
@@ -86,3 +87,16 @@ export const uploadProfileImage = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUserPosts = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const posts = await Post.find({ userRef: req.params.id});
+      res.status(200).json(posts);
+    } catch (error) {
+      next (error)
+    }
+  } else {
+    return next (errorHandler(401, 'You can only view your own posts!'));
+  }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signout, uploadProfileImage,} from '../controllers/user.controller.js';
+import { test, updateUser, deleteUser, signout, uploadProfileImage, getUserPosts,} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 import upload from '../middleware/multer.js';
 
@@ -10,7 +10,7 @@ router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.post('/upload-image', verifyToken, upload.single('image'), uploadProfileImage);
-
+router.get('/posts/:id', verifyToken, getUserPosts)
 
 
 export default router;
