a070d72c85088c74554bd0380ab23b7f2d6b496c
iAmVip1 <gp16373@gmail.com>
2025-08-09 20:18:59 +0545

    comment api route

--- client/src/components/DepartmentRoute.jsx
+++ client/src/components/DepartmentRoute.jsx
@@ -3,5 +3,9 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 export default function DepartmentRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser && currentUser.isHospital ? <Outlet /> : <Navigate to='/not-allowed' />;
+
+  const allowedRoles = ['isHospital', 'isFireDep', 'isPoliceDep', 'isBlood'];
+  const hasAccess = allowedRoles.some(role => currentUser?.[role]);
+
+  return hasAccess ? <Outlet /> : <Navigate to='/not-allowed' />;
 }
