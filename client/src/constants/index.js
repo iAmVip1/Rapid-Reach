391ef155546b1040fc9745a97794e038eee86045
iAmVip1 <gp16373@gmail.com>
2025-11-10 13:38:39 +0545

    homepage

---
+++ client/src/constants/index.js
@@ -0,0 +1,9 @@
+export const words =[
+    { text: 'Ambulance', imgPath:'/images/ideas.svg'},
+    { text: 'Hospital', imgPath:'/images/concepts.svg'},
+    { text: 'Blood Bank', imgPath:'/images/ideas.svg'},
+    { text: 'Police Department', imgPath:'/images/concepts.svg'},
+    { text: 'Police Vehicle', imgPath:'/images/ideas.svg'},
+    { text: 'Fire Department', imgPath:'/images/concepts.svg'},
+    { text: 'Fire Truck', imgPath:'/images/concepts.svg'},
+]
