7c4b72647645ac5d2efe8f27059068b0725b9792
iAmVip1 <gp16373@gmail.com>
2025-11-19 17:03:50 +0545

    comments done

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signout, getUserPosts, getAllUsers, getUserDrives } from '../controllers/user.controller.js';
+import { test, updateUser, deleteUser, signout, getUserPosts, getAllUsers, getUserDrives, getCommentUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -11,6 +11,7 @@ router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/posts/:id', verifyToken, getUserPosts)
 router.get('/drive/:id', verifyToken, getUserDrives)
+router.get('/:userId', getCommentUser)
 
 
 export default router;
